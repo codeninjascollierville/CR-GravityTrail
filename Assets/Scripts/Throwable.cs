12056ab29c32f8910d476cc5fda877eff5e0e0aa
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-08-26 13:46:24 -0500

    Up to 86

--- Assets/Scripts/Throwable.cs
+++ Assets/Scripts/Throwable.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Throwable : MonoBehaviour
 {
     public GameObject shuriken;
+    public Vector3 offset;
+    public int throwableCounter;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,20 @@ public class Throwable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && throwableCounter >= 1)
+        {
+            Vector3 throwablePosition = transform.position + offset;
+            Instantiate(shuriken, throwablePosition, transform.rotation);
+            throwableCounter--;
+        }
+        offset = transform.localScale.x * new Vector3(1, 0, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collectable")
+        {
+            throwableCounter++;
+            Destroy(collision.gameObject);
+        }
     }
 }
